6579759e3af76e9ba0e6178fe223dd4e6b9bc572
tanasak32123 <tanasak@gmail.com>
2026-05-08 22:58:13 +0700

    Remove logging of ID token and headers in request forwarding

--- src/index.ts
+++ src/index.ts
@@ -38,12 +38,10 @@ const app = new Elysia()
 
     const headers = new Headers(request.headers);
     const idToken = await getIdToken(upstream);
-    console.log(`Forwarding request to ${url} with ID token: ${idToken ? "Yes" : "No"}`);
     if (idToken) {
       headers.set("Authorization", `Bearer ${idToken}`);
     }
 
-    console.log(`Forwarding request to ${url} with headers: ${JSON.stringify(Object.fromEntries(headers.entries()))}`);
     const res = await fetch(url, {
       method: request.method,
       headers,
